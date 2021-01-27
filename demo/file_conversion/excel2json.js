ef792ab3c460a957c57b974a75ed998ef0099961
Wsq <wangsq0926@163.com>
2021-01-27 14:02:22 +0800

    excel2json  richText

--- demo/file_conversion/excel2json.js
+++ demo/file_conversion/excel2json.js
@@ -9,14 +9,17 @@ const workbook = new Excel.Workbook();
 workbook.xlsx.readFile(__dirname + '/before/en.xlsx').then(() => {
   const worksheet = workbook.getWorksheet(1);
   worksheet.eachRow((row, rowNum) => {
-    const values = row.values;
-    const keys = values.filter((key, index) => index > 0);
-    jsonDeep(jsonData, keys);
+    // if(rowNum === 25) {
+      const values = row.values;
+      const keys = values.filter((key, index) => index > 0);
+      jsonDeep(jsonData, keys);
+    // }
   });
 
   // console.log(jsonData);
   // return;
-  const jsonStr = JSON.stringify(jsonData, null, '\t');
+  let jsonStr = JSON.stringify(jsonData, null, '\t');
+  jsonStr = `${jsonStr}\n`; // 末尾 空行
   // console.log('>>>>', jsonStr);
   // return;
   fs.writeFile(__dirname + '/after/en1.json', jsonStr, function (err) {
@@ -39,8 +42,17 @@ function jsonDeep(obj, keys) {
     jsonDeep(obj[keys[0]], newKeys);
   }
   if (keys.length === 2) {
+    // console.log(obj, keys, obj[keys[0]]);
     if (!obj[keys[0]]) {
-      obj[keys[0]] = keys[1];
+      let text = '';
+      if(keys[1] instanceof Object) {
+        keys[1].richText.forEach(item => {
+          text = `${text}${item.text}`;
+        });
+      } else {
+        text = keys[1];
+      }
+      obj[keys[0]] = text;
     } else {
       obj[keys[0]][keys[1]] = {};
     }
